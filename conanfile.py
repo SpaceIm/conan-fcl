8be0933cd3fb298146a25d1356a1cf77e4ef5d10
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-04-26 01:30:13 +0200

    delete fPIC option if shared

--- conanfile.py
+++ conanfile.py
@@ -43,6 +43,8 @@ class FclConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         if self.settings.compiler.cppstd:
             tools.check_min_cppstd(self, 11)
         if self.settings.compiler == "Visual Studio" and self.options.shared:
